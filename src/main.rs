3f4c75a060f9cc945b85fe4d2260f369cca1417e
Noah Garland <noahgarland02@gmail.com>
2024-05-17 14:42:26 -0500

    Added extra line for debugging

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ fn main() {
         Ok(cfg) => Some(cfg),
         Err(e) => {
             println!("Error: {}", e);
+            println!("Using default device instead");
             None
         }
     };
